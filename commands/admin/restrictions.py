c19ebdaf1ec34955734b941647889cfab93a2f7e
Raxorr-69 <yks324232@gmail.com>
2026-09-13 14:49:56 +0530

    Update restrictions.py

--- commands/admin/restrictions.py
+++ commands/admin/restrictions.py
@@ -19,7 +19,7 @@ class Restrictions(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="imageonly")
+    @commands.hybird_command(name="imageonly")
     @admin_only()
     @commands.guild_only()
     @commands.cooldown(5, 10, commands.BucketType.user)
@@ -76,7 +76,7 @@ class Restrictions(commands.Cog):
             )
         )
 
-    @commands.command(name="clipsonly")
+    @commands.hybird_command(name="clipsonly")
     @admin_only()
     @commands.guild_only()
     @commands.cooldown(5, 10, commands.BucketType.user)
