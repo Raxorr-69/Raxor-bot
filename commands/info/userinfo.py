b3b440c4fa0e2d92fda1d293224b6860de7b1354
Raxorr-69 <yks324232@gmail.com>
2026-09-13 14:11:08 +0530

    Change command decorator from command to hybrid_command

--- commands/info/userinfo.py
+++ commands/info/userinfo.py
@@ -12,7 +12,7 @@ class UserInfo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="userinfo")
+    @commands.hybird_command(name="userinfo")
     @commands.guild_only()
     @commands.cooldown(2, 5, commands.BucketType.user)
     async def userinfo(
